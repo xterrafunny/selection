ba87c4d2b71ce62ba84b15c63916f1f3c7f13eca
xterrafunny <gaibov.dr@phystech.edu>
2020-04-13 23:15:14 +0300

    main.cpp comment removed

--- main.cpp
+++ main.cpp
@@ -5,15 +5,3 @@ int main() {
   Helper helper;
   helper.processQueries(std::cin, std::cout);
 }
-
-/*
-add_words 3
-yabloko yabloko yabloneviy
-query yablo
-add_letters n
-complete
-add_word yablonya
-add_word yablonya
-add_word yablonya
-query yabl
-*/
